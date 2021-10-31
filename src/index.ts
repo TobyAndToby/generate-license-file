44b9847e945199e63525d9c966156bdea73cf6ce
Toby Smith <tobysmith568@hotmail.co.uk>
2021-10-31 21:52:44 +0000

    Document the possible options

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,32 @@ import { join } from "path/posix";
 import { Compiler, WebpackError } from "webpack";
 
 interface Options {
+  /**
+   * The file name for the file containing the licenses.
+   */
   outputFileName: string;
+
+  /**
+   * The folder path within the project's output folder where the licenses file will be written.
+   */
   outputFolder: string;
+
+  /**
+   * The folder containing the project's package.json.
+   * Defaults to the webpack context directory which is usually sufficient for most setups.
+   */
   projectFolder: string;
+
+  /**
+   * Configures the plugin to use a dev-only placeholder text value.
+   * Useful to help speed up dev compile times.
+   */
   isDev: boolean;
+
+  /**
+   * The line ending to use when writing the licenses file.
+   * Defaults to the line ending of the operating system.
+   */
   lineEnding?: LineEnding;
 }
 
