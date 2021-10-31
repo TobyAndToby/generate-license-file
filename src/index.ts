b0ed859ecb4300fca3d231fb11dd9499c7d5d088
Toby Smith <tobysmith568@hotmail.co.uk>
2021-10-31 21:12:13 +0000

    Install Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import { Compiler } from "webpack";
 import { getLicenseFileText, LineEnding } from "generate-license-file";
 import { join } from "path/posix";
+import { Compiler } from "webpack";
 
 interface Options {
-	outputFileName: string;
+  outputFileName: string;
   outputFolder: string;
-  isDev: boolean
-  lineEnding?: LineEnding
+  isDev: boolean;
+  lineEnding?: LineEnding;
 }
 
-const devImplementation = () => Promise.resolve(`In a production build this file will contain the licenses of your production dependencies.
+const devImplementation = () =>
+  Promise.resolve(`In a production build this file will contain the licenses of your production dependencies.
 
 For dev builds it only contains this text for the sake of build speed.`);
 
 class LicenseFilePlugin {
-	private pluginName = LicenseFilePlugin.name;
+  private pluginName = LicenseFilePlugin.name;
 
   private static defaultOptions: Options = {
     outputFileName: "third-party-licenses.txt",
@@ -23,7 +24,7 @@ class LicenseFilePlugin {
     lineEnding: undefined
   };
 
-	private options: Options;
+  private options: Options;
 
   constructor(options: Partial<Options>) {
     this.options = { ...LicenseFilePlugin.defaultOptions, ...options };
@@ -34,12 +35,12 @@ class LicenseFilePlugin {
     const { Compilation } = webpack;
     const { RawSource } = webpack.sources;
     const { outputFileName, outputFolder, isDev, lineEnding } = this.options;
-		
-    compiler.hooks.thisCompilation.tap(this.pluginName, (compilation) => {
+
+    compiler.hooks.thisCompilation.tap(this.pluginName, compilation => {
       compilation.hooks.processAssets.tapAsync(
         {
           name: this.pluginName,
-          stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL,
+          stage: Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL
         },
         (_, resolve) => {
           const projectFolder = compiler.context;
@@ -47,11 +48,8 @@ class LicenseFilePlugin {
 
           const implementation = isDev ? devImplementation : getLicenseFileText;
 
-					implementation(projectFolder, lineEnding).then(text => {
-            compilation.emitAsset(
-              outputPath,
-              new RawSource(text)
-            );
+          implementation(projectFolder, lineEnding).then(text => {
+            compilation.emitAsset(outputPath, new RawSource(text));
             resolve();
           });
         }
