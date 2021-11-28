835143108e8354cef008bbc7b70a176225dfdb3d
Toby Smith <tobysmith568@hotmail.co.uk>
2021-11-28 22:01:47 +0000

    Refactor getProjectLicensesInternal

--- src/internal/getProjectLicensesInternal.ts
+++ src/internal/getProjectLicensesInternal.ts
@@ -1,4 +1,6 @@
+import { ModuleInfo } from "license-checker";
 import { License } from "../models/license";
+import console from "../utils/console.utils";
 import { doesFileExist, doesFolderExist, readFileAsync } from "../utils/file.utils";
 import { getProject, Project } from "../utils/licence.utils";
 
@@ -6,46 +8,72 @@ const UTF8 = "utf-8";
 
 export async function getProjectLicensesInternal(path: string): Promise<License[]> {
   try {
-    const dependencyLicenses: Map<string, string[]> = new Map<string, string[]>();
-
-    if (!(await doesFolderExist(path))) {
-      throw new Error("Cannot find directory " + path);
-    }
-
-    const project: Project = await getProject({
-      start: path,
-      production: true
-    });
-
-    for (const [dependencyName, dependencyValue] of Object.entries(project)) {
-      if (!!dependencyValue.licenseFile) {
-        let license: string = "";
-
-        if (await doesFileExist(dependencyValue.licenseFile)) {
-          license = await readFileAsync(dependencyValue.licenseFile, { encoding: UTF8 });
-        } else {
-          // If we cannot find the license text, we use the license type as a fallback
-          const { licenses } = dependencyValue;
-          if (typeof licenses !== "undefined" && licenses.length > 0) {
-            license = `(${typeof licenses === "string" ? licenses : licenses[0]})`;
-          }
-        }
-
-        if (!dependencyLicenses.has(license)) {
-          dependencyLicenses.set(license, []);
-        }
-
-        dependencyLicenses.get(license)?.push(dependencyName);
-      }
-    }
+    const dependencyLicenses = await getDependencyMapForProject(path);
 
     const licences: License[] = [];
     for (const [license, dependencies] of dependencyLicenses) {
       licences.push(new License(license, dependencies));
     }
+
     return licences;
   } catch (error) {
     console.error(error.message);
     return Promise.reject();
   }
 }
+
+const getDependencyMapForProject = async (path: string) => {
+  if (!(await doesFolderExist(path))) {
+    throw new Error("Cannot find directory " + path);
+  }
+
+  const project: Project = await getProject({
+    start: path,
+    production: true
+  });
+
+  const projectDependencies = groupProjectDependenciesByLicenseText(project);
+  return projectDependencies;
+};
+
+const groupProjectDependenciesByLicenseText = async (project: Project) => {
+  const dependencyLicenses: Map<string, string[]> = new Map<string, string[]>();
+
+  for (const [dependencyName, dependencyValue] of Object.entries(project)) {
+    if (!dependencyValue.licenseFile) {
+      continue;
+    }
+
+    const license: string = await getLicenseContent(dependencyValue);
+
+    if (!dependencyLicenses.has(license)) {
+      dependencyLicenses.set(license, []);
+    }
+
+    dependencyLicenses.get(license)?.push(dependencyName);
+  }
+
+  return dependencyLicenses;
+};
+
+const getLicenseContent = async (moduleInfo: ModuleInfo) => {
+  const licenseFilePath: string | undefined = moduleInfo.licenseFile;
+
+  if (!!licenseFilePath && (await doesFileExist(licenseFilePath))) {
+    return await readFileAsync(licenseFilePath, { encoding: UTF8 });
+  }
+
+  return getLicenseType(moduleInfo);
+};
+
+const getLicenseType = (moduleInfo: ModuleInfo) => {
+  const { licenses } = moduleInfo;
+
+  if (!!licenses && licenses.length > 0) {
+    const licenceType = typeof licenses === "string" ? licenses : licenses[0];
+    return `(${licenceType})`;
+  }
+
+  console.warn(`No license found for ${moduleInfo.name}!`);
+  return "Unknown Licence!";
+};
