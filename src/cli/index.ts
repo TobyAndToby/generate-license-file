039b37e83c98eb8f922e4fb4afddccd2d35ebcfe
Toby Smith <tobysmith568@hotmail.co.uk>
2021-12-12 17:35:10 +0000

    Refactor the cli

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -8,55 +8,6 @@ import { doesFileExist } from "../utils/file.utils";
 import { IArguments } from "./arguments.interface";
 import { validArguments } from "./valid-arguments";
 
-function parseArgumentsIntoOptions(rawArgs: string[]): IArguments {
-  const args: arg.Result<any> = arg(validArguments, {
-    argv: rawArgs.slice(2)
-  });
-
-  return {
-    input: args["--input"] || undefined,
-    output: args["--output"] || undefined,
-    overwriteOutput: args["--overwrite"] || undefined,
-    eol: args["--eol"] || undefined
-  };
-}
-
-async function promptForAnswers(options: IArguments): Promise<IArguments> {
-  if (!options.input) {
-    const answer: any = await prompt({
-      type: "input",
-      name: "input",
-      initial: (await doesFileExist("./package.json")) ? "./package.json" : "",
-      message: "package.json location:"
-    });
-
-    options.input = answer.input;
-  }
-
-  while (!options.output || options.overwriteOutput === false) {
-    const answer: any = await prompt({
-      type: "input",
-      name: "output",
-      initial: "3rd-party-licenses.txt",
-      message: "Output file location:"
-    });
-
-    options.output = answer.output;
-
-    if (await doesFileExist(options.output)) {
-      const yesNoAnswer: any = await prompt({
-        type: "confirm",
-        name: "overwriteOutput",
-        message: "The given output file already exists and will be overwritten. Is this OK?"
-      });
-
-      options.overwriteOutput = yesNoAnswer.overwriteOutput;
-    }
-  }
-
-  return options;
-}
-
 export async function cli(args: string[]): Promise<void> {
   let options: IArguments = parseArgumentsIntoOptions(args);
   options = await promptForAnswers(options);
@@ -73,3 +24,71 @@ export async function cli(args: string[]): Promise<void> {
 
   spinner.stop();
 }
+
+function parseArgumentsIntoOptions(rawArgs: string[]): IArguments {
+  const args: arg.Result<any> = arg(validArguments, {
+    argv: rawArgs.slice(2)
+  });
+
+  return {
+    input: args["--input"] || undefined,
+    output: args["--output"] || undefined,
+    overwriteOutput: args["--overwrite"] || undefined,
+    eol: args["--eol"] || undefined
+  };
+}
+
+async function promptForAnswers(options: IArguments): Promise<IArguments> {
+  if (!options.input) {
+    options = await promptForInput(options);
+  }
+
+  let outputFileExists: boolean = await doesFileExist(options.output);
+  while (!options.output || (outputFileExists && !options.overwriteOutput)) {
+    options = await promptForOutput(options);
+
+    outputFileExists = await doesFileExist(options.output);
+    if (outputFileExists) {
+      options = await promptForOverwrite(options);
+    }
+  }
+
+  return options;
+}
+
+const promptForInput = async (options: IArguments) => {
+  const packageJsonExists = await doesFileExist("./package.json");
+
+  const answer = await prompt<IArguments>({
+    type: "input",
+    name: "input",
+    initial: packageJsonExists ? "./package.json" : "",
+    message: "package.json location:"
+  });
+
+  options.input = answer.input;
+  return options;
+};
+
+const promptForOutput = async (options: IArguments) => {
+  const outputAnswer = await prompt<IArguments>({
+    type: "input",
+    name: "output",
+    initial: "3rd-party-licenses.txt",
+    message: "Output file location:"
+  });
+
+  options.output = outputAnswer.output;
+  return options;
+};
+
+const promptForOverwrite = async (options: IArguments) => {
+  const doesFileExistAnswer = await prompt<IArguments>({
+    type: "confirm",
+    name: "overwriteOutput",
+    message: "The given output file already exists and will be overwritten. Is this OK?"
+  });
+
+  options.overwriteOutput = doesFileExistAnswer.overwriteOutput;
+  return options;
+};
