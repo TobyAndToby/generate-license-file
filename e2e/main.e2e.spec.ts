6b5cf72030357463778843a094cb1f7b30529b05
Toby Smith <tobysmith568@hotmail.co.uk>
2021-12-17 22:35:44 +0000

    Update the e2e tests

--- e2e/main.e2e.spec.ts
+++ e2e/main.e2e.spec.ts
@@ -5,15 +5,16 @@ import { getProjectLicenses } from "../src/main";
 
 const execAsync = promisify(exec);
 
-const testFiles = ["./all-have-licenses", "./missing-license-file"];
+const testFiles = ["./all-have-licenses/package.json", "./missing-license-file/package.json"];
 
 describe("main", () => {
   describe("getProjectLicenses", () => {
     testFiles.forEach(file =>
       it(`should match snapshot for: ${file}`, async () => {
         const fullPath = path.join(__dirname, file);
+        const directory = path.dirname(fullPath);
 
-        await execAsync("npm ci", { cwd: fullPath });
+        await execAsync("npm ci", { cwd: directory });
         const result = await getProjectLicenses(fullPath);
 
         expect(result).toMatchSnapshot();
