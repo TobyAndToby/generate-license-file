753bc64eda9b40fa4ccc8e721fe5a430fb44fef6
Toby Smith <tobysmith568@hotmail.co.uk>
2022-02-12 21:58:51 +0000

    Set the jest test timeout to 10 seconds

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/*.spec.ts"],
-  collectCoverageFrom: ["src/**/*.ts"]
+  collectCoverageFrom: ["src/**/*.ts"],
+  testTimeout: 10000
 };
