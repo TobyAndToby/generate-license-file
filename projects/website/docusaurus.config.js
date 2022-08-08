2b457844054de2b629213c941189658d761ed5d0
Toby Bessant <toby.bessant.1998@gmail.com>
2022-08-08 19:18:26 +0100

    Fix code snippet theme on docs pages

--- projects/website/docusaurus.config.js
+++ projects/website/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const lightCodeTheme = require("prism-react-renderer/themes/vsDark");
+const darkCodeTheme = require("prism-react-renderer/themes/vsLight");
 const path = require("path");
 
 /** @type {import('@docusaurus/types').Config} */
