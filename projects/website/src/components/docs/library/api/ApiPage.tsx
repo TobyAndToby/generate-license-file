d733e3e755b314501879313cf71f92a7d9ddbd29
Toby Smith <tobysmith568@hotmail.co.uk>
2022-08-21 13:47:39 +0100

    Move the language toggle from the navbar to the API page

--- projects/website/src/components/docs/library/api/ApiPage.tsx
+++ projects/website/src/components/docs/library/api/ApiPage.tsx
@@ -1,3 +1,4 @@
+import TypeScriptToggle from "@site/src/components/TypeScriptToggle";
 import React, { FC } from "react";
 import { getLibraryMethods } from "./getLibraryMethods";
 import LibraryMethod from "./LibraryMethod";
@@ -10,6 +11,7 @@ interface Props {
 const ApiPage: FC<Props> = ({ librarySchema }) => {
   return (
     <>
+      <TypeScriptToggle />
       <h2>Methods</h2>
       {getLibraryMethods(librarySchema).map(method => (
         <LibraryMethod key={method.name} method={method} />
