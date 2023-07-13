654d9e034c986674f1353670a814652d8682ec40
Toby Smith <tobysmith568@hotmail.co.uk>
2023-07-13 19:36:05 +0100

    Add the --version flag and update flag descriptions

--- src/packages/generate-license-file/src/lib/cli/commands/main.ts
+++ src/packages/generate-license-file/src/lib/cli/commands/main.ts
@@ -6,6 +6,9 @@ import { Eol } from "../args/eol";
 import { LineEnding } from "../../lineEndings";
 import { generateLicenseFile } from "../../generateLicenseFile";
 import { spinner } from "../spinner";
+import { join } from "path";
+import logger from "../../utils/console.utils";
+import { readPackageJson } from "../../utils/packageJson.utils";
 
 export type CombinedConfig = {
   inputs?: string[];
@@ -25,15 +28,36 @@ export interface ProgramOptions {
 
 export const mainCommand = new Command()
   .name("generate-license-file")
-  .description("// TODO")
-  .option("-c,--config <path>", "")
-  .option("-i,--input <paths...>", "")
-  .option("-o,--output <path>", "")
-  .option("--eol <eol>", "")
-  .option("--ci", "")
-  .option("--no-spinner", "")
-  .option("--overwrite", "")
+  .description(
+    "Generates a text file containing all of the licenses for your production dependencies",
+  )
+  .helpOption("-h,--help", "Display help text")
+  .option(
+    "-c,--config <path>",
+    "Specify a path to a generate-license-file config file. Files will be automatically detected if this flag is not given",
+  )
+  .option("-i,--input <paths...>", "Specify one or more paths to package.json files to include")
+  .option("-o,--output <path>", "Specify the path of the file to be created")
+  .option(
+    "--overwrite",
+    "Indicates that the output file should be overwritten if it already exists",
+  )
+  .option(
+    "--eol <eol>",
+    "Specify a particular line ending to use in the output file. Otherwise, the line ending of the current OS will be used",
+  )
+  .option("--ci", "Fail with a non-zero exit code if user input is required")
+  .option(
+    "--no-spinner",
+    "Don't show the progress spinner while generating the license file. This is implicitly true if --ci is given",
+  )
+  .option("-v,--version", "Prints the installed version of generate-license-file")
   .action(async givenArgs => {
+    if (givenArgs.version) {
+      printPackageVersion();
+      return;
+    }
+
     const cliArgs = {
       // Commander sets spinner to true by default, meaning it will always override
       // a spinner value in the config file - set to undefined if it is true (ie,
@@ -123,3 +147,9 @@ function getMessageFromCaughtUnknown(e: unknown): string {
 
   return "Unknown error";
 }
+
+const printPackageVersion = async (): Promise<void> => {
+  const packageJsonLocation = join(__dirname, "../../../../package.json");
+  const { version } = await readPackageJson(packageJsonLocation);
+  logger.log(`v${version}`);
+};
