e5e78970c77933e64d67a9c0e9b21af165b13f12
Toby Smith <tobysmith568@hotmail.co.uk>
2023-07-15 21:27:29 +0100

    Strengthen lineEnding types

--- src/packages/generate-license-file/src/lib/lineEndings.ts
+++ src/packages/generate-license-file/src/lib/lineEndings.ts
@@ -1,13 +1,6 @@
 import os from "os";
 
-const lineEndingValues = ["\r\n", "\n"] as const;
-
-export type LineEndingCharacters = typeof lineEndingValues[number];
-
-const lineEndings = {
-  crlf: "\r\n",
-  lf: "\n",
-} satisfies Record<string, LineEndingCharacters>;
+export const lineEndings = ["crlf", "lf"] as const;
 
 /**
  * Used to specify which line endings to use in the generated file.
@@ -18,16 +11,22 @@ const lineEndings = {
  *
  * `lf` (`\n`)
  */
-export type LineEnding = keyof typeof lineEndings;
+export type LineEnding = typeof lineEndings[number];
 
-export const allLineEndings = Object.keys(lineEndings) as LineEnding[];
+const lineEndingsMap = {
+  crlf: "\r\n",
+  lf: "\n",
+} as const satisfies Record<LineEnding, string>;
+
+export type LineEndingCharacters = typeof lineEndingsMap[LineEnding];
+export const lineEndingCharacters = Object.values(lineEndingsMap);
 
 export const isLineEnding = (input: string | undefined): input is LineEnding => {
   if (input === undefined) {
     return true;
   }
 
-  return allLineEndings.includes(input as LineEnding);
+  return lineEndings.includes(input as LineEnding);
 };
 
 export const getLineEndingCharacters = (input: LineEnding | undefined): LineEndingCharacters => {
@@ -35,11 +34,5 @@ export const getLineEndingCharacters = (input: LineEnding | undefined): LineEndi
     return os.EOL as LineEndingCharacters;
   }
 
-  const lineEndingValue = lineEndings[input as LineEnding];
-
-  if (!lineEndingValue) {
-    throw new Error(`Unknown line ending value: ${input}`);
-  }
-
-  return lineEndingValue;
+  return lineEndingsMap[input];
 };
