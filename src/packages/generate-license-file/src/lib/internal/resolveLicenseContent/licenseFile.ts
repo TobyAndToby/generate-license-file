17e61c234e896d4afe4460e881157ed53dd8dce8
Toby Smith <tobysmith568@hotmail.co.uk>
2023-11-14 11:47:44 +0000

    Update the warnings given when there are multiple licenses

--- src/packages/generate-license-file/src/lib/internal/resolveLicenseContent/licenseFile.ts
+++ src/packages/generate-license-file/src/lib/internal/resolveLicenseContent/licenseFile.ts
@@ -29,13 +29,20 @@ export const licenseFile: Resolution = async inputs => {
   }
 
   if (filteredLicenseFiles.length > 1) {
-    const relativeLicenseFiles = filteredLicenseFiles.map(file => relative(directory, file));
-
-    logger.warn(
-      `Found multiple license files for ${packageJson.name}@${
-        packageJson.version
-      }: ${relativeLicenseFiles.join(", ")}`,
+    const relativeLicenseFiles = filteredLicenseFiles.map(
+      file => ` - ./${relative(directory, file)}`,
     );
+
+    const warningLines = [
+      `Found multiple license files for ${packageJson.name}@${packageJson.version}:`,
+      ...relativeLicenseFiles,
+      "We suggest you determine which file you wish to use and replace the license content",
+      `for ${packageJson.name}@${packageJson.version} using a generate-license-file config file.`,
+      "See: https://generate-license-file.js.org/docs/cli/config-file for more information.",
+      "", // Empty line for spacing
+    ];
+
+    logger.warn(warningLines.join("\n"));
   }
 
   return await readFile(filteredLicenseFiles[0], { encoding: "utf-8" });
