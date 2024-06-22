2e1ab7f7e1bf23468f3d9887bf53f93819c66f31
Toby Bessant <toby.bessant.1998@gmail.com>
2024-06-22 16:23:39 +0100

    Update http location

--- e2e/config-file/test/cli/replacement/config.js
+++ e2e/config-file/test/cli/replacement/config.js
@@ -9,6 +9,6 @@ module.exports = {
     "dep-three": "./some-path-that-we-dont-want-to-use.txt",
     "dep-three@1.0.0": "./name-and-version-replacement-content.txt",
     "dep-four":
-      "https://raw.githubusercontent.com/TobyAndToby/generate-license-file/c671e5b374285868866ccdb64b42e74237240c12/e2e/.remote-licenses/license.md",
+      "https://raw.githubusercontent.com/TobyAndToby/generate-license-file/main/e2e/.remote-licenses/license.md",
   },
 };
